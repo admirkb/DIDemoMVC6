5799160f62c189b3c9fa63bf13261db40391b1a9
kelvinbeech <kelvin.beech@kjb.co.uk>
2015-12-22 17:16:13 +0000

    Initial

---
+++ src/DependenceyInjectionDemo/Services/GreetingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependenceyInjectionDemo.Services
+{
+    public class GreetingService:IGreetingService
+    {
+
+        public string GetGreeting()
+        {
+
+            return "Wales are at the euro's !!!" + " " + GetHashCode();
+        }
+    }
+}
